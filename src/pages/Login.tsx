9b6e42322edfae45b2590902be3b9f99931443cb
Sanjida Akter <sanjidakter2304@gmail.com>
2024-07-05 16:50:43 +0600

    login successful and cookie is being added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: 'A-0001',
+      password: 'admin123',
+    },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data=>", data);
+  console.log("error=>", error);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,  // Corrected the typo here
+    };
+    login(userInfo);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
