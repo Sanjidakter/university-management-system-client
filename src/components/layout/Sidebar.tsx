2c2f421d5e8027288dc1d46d66b57ffb3a6d0502
Sanjida Akter <sanjidakter2304@gmail.com>
2024-08-07 11:19:29 +0600

    creating the add student form

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,21 +1,22 @@
-import { Layout, Menu } from "antd";
-import sidebarItemsGenerator from "../../utils/sidebarItemsGenerator";
-import { adminPaths } from "../../routes/admin.routes";
-import { facultyPaths } from "../../routes/faculty.routes";
-import { studentPaths } from "../../routes/student.routes";
-import { useAppSelector } from "../../redux/hooks";
-import { selectCurrentUser } from "../../redux/features/auth/authSlice";
+import { Layout, Menu } from 'antd';
+import { adminPaths } from '../../routes/admin.routes';
+import { facultyPaths } from '../../routes/faculty.routes';
+import { studentPaths } from '../../routes/student.routes';
+import { useAppSelector } from '../../redux/hooks';
+import { selectCurrentUser } from '../../redux/features/auth/authSlice';
+import sidebarItemsGenerator from '../../utils/sidebarItemsGenerator';
 
 const { Sider } = Layout;
 
 const userRole = {
-  ADMIN :"admin",
-  FACULTY :"faculty",
-  STUDENT :"student",
+  ADMIN: 'admin',
+  FACULTY: 'faculty',
+  STUDENT: 'student',
 };
 
 const Sidebar = () => {
   const user = useAppSelector(selectCurrentUser);
+
   let sidebarItems;
 
   switch (user!.role) {
@@ -26,7 +27,7 @@ const Sidebar = () => {
       sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
       break;
     case userRole.STUDENT:
-      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.ADMIN);
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
       break;
 
     default:
@@ -37,28 +38,23 @@ const Sidebar = () => {
     <Sider
       breakpoint="lg"
       collapsedWidth="0"
-      onBreakpoint={(broken) => {
-        // console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        // console.log(collapsed, type);
-      }}
+      style={{ height: '100vh', position: 'sticky', top: '0', left: '0' }}
     >
       <div
         style={{
-          color: "white",
-          height: "4rem",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          color: 'white',
+          height: '4rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <h1 style={{ height: "20px" }}> PH Uni</h1>
+        <h1>PH Uni</h1>
       </div>
       <Menu
         theme="dark"
         mode="inline"
-        defaultSelectedKeys={["4"]}
+        defaultSelectedKeys={['4']}
         items={sidebarItems}
       />
     </Sider>
--- src/constants/global.ts
+++ src/constants/global.ts
@@ -17,3 +17,18 @@ export const monthNames = [
     value: item,
     label: item,
   }));
+
+  export const gender = ['Male','Female','Other'];
+
+  export const genderOptions = gender.map((item)=>
+  (  {
+    value:item.toLocaleLowerCase(),
+    label:item,
+ }));
+
+ export const bloodGroups=['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
+
+export const bloodGroupOptions = bloodGroups.map((item)=>({
+  value:item,
+  label:item,
+}))
